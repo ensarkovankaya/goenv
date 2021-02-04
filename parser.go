43acc4f98bb2c2d3f9d2cfaa63a0836b849d705e
ensarkovankaya <ensar@kovankaya.com>
2021-02-04 13:40:15 +0300

    Add parser

---
+++ parser.go
@@ -0,0 +1,25 @@
+package goenv
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+// Find an environment variable in
+func find(env string) (string, bool) {
+	for _, value := range os.Environ() {
+		if strings.HasPrefix(value, env) {
+			return value[len(value)+1:], true
+		}
+	}
+	return "", false
+}
+
+func parse(env string, required bool) (string, bool) {
+	value, exists := find(env)
+	if !exists && required {
+		panic(fmt.Sprintf("environment variable not defined: %v", env))
+	}
+	return value, exists
+}
